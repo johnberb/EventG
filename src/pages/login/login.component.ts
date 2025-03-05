c6768e1e5cdae40938488db6ad486abc7636033f
johnberb <rubengs.j@gmail.com>
2025-03-05 19:39:46 +0300

    first commit

---
+++ src/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+RouterOutlet
+@Component({
+    templateUrl:'./login.component.html',
+    styleUrl:'./login.component.css'
+
+}) 
+
+ export class LoginComponent {
+    
+ }
